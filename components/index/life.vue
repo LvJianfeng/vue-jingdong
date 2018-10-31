6043ac740a240a4e3c759fe30f94e65d0ede1d7d
GanYihuan <583520052@qq.com>
2018-11-01 03:04:33 +0800

    登录界面

--- components/index/life.vue
+++ components/index/life.vue
@@ -54,7 +54,6 @@ export default {
 }
 </script>
 
-
 <style lang="scss">
 @import '@/assets/css/index/life.scss';
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -24,5 +24,5 @@ export default {
 </script>
 
 <style lang='scss'>
-@import url("../assets/css/reset.css");
+@import url('../assets/css/reset.css');
 </style>
