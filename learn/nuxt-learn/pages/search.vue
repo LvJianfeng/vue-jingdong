dac87af9d3afe6abfec14860c22567ddeeea6fde
GanYihuan <583520052@qq.com>
2018-10-31 22:39:28 +0800

    add learn

---
+++ learn/nuxt-learn/pages/search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+    Page is search
+    <ul>
+      <li v-for="(item, idx) in $store.state.navbar.app" :key="idx">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+	layout: 'search',
+	data() {
+		return {
+			list: []
+		}
+	},
+	/*
+  asyncData: nuxt 生命周期,
+  ssr 服务器把编译好的内容下发给你
+  异步获取的数据同时给浏览器端
+  */
+	async asyncData() {
+		let {
+			status,
+			data: { list }
+		} = await axios.get('http://localhost:3000/city/list')
+		if (status === 200) {
+			return {
+				list
+			}
+		}
+	}
+}
+</script>
---
+++ learn/nuxt-learn/store/modules/navbar.js
@@ -0,0 +1,20 @@
+const state = () => ({
+  app: ['a', 'c']
+})
+const mutations = {
+  add(state, text) {
+    state.app.push(text)
+  }
+}
+const actions = {
+  add: ({ commit }, text) => {
+    commit('add', text)
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
