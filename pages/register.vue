bd2b2090105b7c2ad2c925f215e6b4a7cee4c549
GanYihuan <583520052@qq.com>
2019-01-03 10:53:36 +0800

    8-1

--- pages/register.vue
+++ pages/register.vue
@@ -12,8 +12,7 @@
             <el-button
               type="primary"
               size="small"
-            >登录
-            </el-button>
+            >登录</el-button>
           </a>
         </span>
       </header>
@@ -28,51 +27,62 @@
       >
         <el-form-item
           label="昵称"
-          prop="name">
+          prop="name"
+        >
           <el-input v-model="ruleForm.name"/>
         </el-form-item>
         <el-form-item
           label="邮箱"
-          prop="email">
+          prop="email"
+        >
           <el-input v-model="ruleForm.email"/>
           <el-button
             size="mini"
             round
-            @click="sendMsg">发送验证码</el-button>
+            @click="sendMsg"
+          >发送验证码</el-button>
           <span class="status">{{ statusMsg }}</span>
         </el-form-item>
         <el-form-item
           label="验证码"
-          prop="code">
+          prop="code"
+        >
           <el-input
             v-model="ruleForm.code"
-            maxlength="4"/>
+            maxlength="4"
+          />
         </el-form-item>
         <el-form-item
           label="密码"
-          prop="pwd">
+          prop="pwd"
+        >
           <el-input
             v-model="ruleForm.pwd"
-            type="password"/>
+            type="password"
+          />
         </el-form-item>
         <el-form-item
           label="确认密码"
-          prop="cpwd">
+          prop="cpwd"
+        >
           <el-input
             v-model="ruleForm.cpwd"
-            type="password"/>
+            type="password"
+          />
         </el-form-item>
         <el-form-item>
           <el-button
             type="primary"
-            @click="register">同意以下协议并注册</el-button>
+            @click="register"
+          >同意以下协议并注册</el-button>
           <div class="error">{{ error }}</div>
         </el-form-item>
         <el-form-item>
           <a
             class="f1"
             href="http://www.meituan.com/about/terms"
-            target="_blank">《美团网用户协议》</a>
+            target="_blank"
+          >《美团网用户协议》</a>
         </el-form-item>
       </el-form>
     </section>
