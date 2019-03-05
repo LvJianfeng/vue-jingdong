0435d77a4efc89c74b03c775cd681a1027d9311e
GanYihuan <583520052@qq.com>
2019-03-05 17:21:57 +0800

    logo

--- components/public/header/logo.vue
+++ components/public/header/logo.vue
@@ -1,12 +1,27 @@
 ﻿<template>
   <div class="m-logo">
     <!-- 美团图标 -->
-    <nuxt-link to="/">
+    <!-- <nuxt-link to="/">
       <img
         src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
         alt="美团"
       >
-    </nuxt-link>
+    </nuxt-link> -->
+    <ul class="nav">
+      <li class="list">
+        <dl>
+          <dd>
+            <nuxt-link to="/">
+              <img
+                src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
+                alt="美团"
+              >
+            </nuxt-link>
+          </dd>
+        </dl>
+      </li>
+    </ul>
+
   </div>
 </template>
 
