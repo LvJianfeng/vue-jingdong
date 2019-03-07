a40765d0a30ef81428c98c31501f2dccc853c01c
GanYihuan <583520052@qq.com>
2019-03-07 21:14:42 +0800

    logo

--- components/public/header/logo.vue
+++ components/public/header/logo.vue
@@ -2,23 +2,35 @@
   <div class="m-logo">
     <ul class="nav">
       <li class="list">
-        <dl>
-          <dd>
-            <nuxt-link to="/">
-              <img
-                src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
-                alt="美团"
-              >
-            </nuxt-link>
-          </dd>
-        </dl>
+        <template v-if="logoType">
+          <dl>
+            <dd>
+              <nuxt-link to="/">
+                <img
+                  src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
+                  alt="美团"
+                >
+              </nuxt-link>
+            </dd>
+          </dl>
+        </template>
+        <template v-else>
+          <i class="el-icon-location"/>
+          {{ $store.state.geo.position.city }}
+        </template>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    logoType() {
+      return this.$route.name === 'index'
+    }
+  }
+}
 </script>
 
 <style lang="scss">
